cb883d44c6ef630f91ec536496c8960e3ffb202b
il07621 <il07621@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-03-06 10:04:28 +0000

    Début de création de la classe Ultrason

--- trunk/src/capteurs/Ultrason.java
+++ trunk/src/capteurs/Ultrason.java
@@ -1,5 +1,35 @@
 package capteurs;
 
 public class Ultrason {
-
+	
+	//Attributs 
+	//float car utilisé par leJOS
+	private static float[] distance;
+	private static boolean bruit_detecte;
+	//pour savoir si le capteur est effectivement actif
+	private static boolean status;
+	
+	//Méthodes
+	//Modifient status
+	public static void startScan() {
+		
+	}
+	public static void stopScan() {
+		
+	}
+	//Donne la valuer de status
+	public static boolean getStatus() {
+		return status;
+	}
+	//Renvoie la distance mesurée par le capteur
+	public static float getDistance() {
+		float[] distance = new float[Capteur.ULTRASON.sampleSize()];
+		return distance[0];
+	}
+	//Détecte la présence d'un autre robot
+	public static boolean autreRobot() {
+		boolean autreRobot=false;
+		return autreRobot;
+	}
+	
 }
