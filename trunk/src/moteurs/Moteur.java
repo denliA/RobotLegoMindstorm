ec7196b444afd5367b592b1585b22c81856c6cba
il07621 <il07621@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-03-06 11:52:27 +0000

    Création de la classe Moteur

--- trunk/src/moteurs/Moteur.java
+++ trunk/src/moteurs/Moteur.java
@@ -1,5 +1,18 @@
 package moteurs;
 
-public class Moteur {
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 
+public class Moteur {
+	public static EV3LargeRegulatedMotor MOTEUR_GAUCHE = new EV3LargeRegulatedMotor(MotorPort.B);
+	public static EV3LargeRegulatedMotor MOTEUR_DROIT = new EV3LargeRegulatedMotor(MotorPort.A);
+	public static EV3MediumRegulatedMotor MOTEUR_PINCE = new EV3MediumRegulatedMotor(MotorPort.C);
+	
+	public static void ouvrirMoteurs() {
+		//TO DO
+	}
+	public static void fermerMoteurs() {
+		//TO DO
+	}
 }
