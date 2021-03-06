0a94052da5325d58dba7862d0b1d78b0b91d3537
il07621 <il07621@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-03-06 10:35:09 +0000

    Complétion de la Classe Ultrason

--- trunk/src/capteurs/Ultrason.java
+++ trunk/src/capteurs/Ultrason.java
@@ -4,18 +4,18 @@ public class Ultrason {
 	
 	//Attributs 
 	//float car utilisé par leJOS
-	private static float[] distance;
-	private static boolean bruit_detecte;
+	private static float distance;
+	private static boolean bruitDetecte;
 	//pour savoir si le capteur est effectivement actif
 	private static boolean status;
 	
 	//Méthodes
 	//Modifient status
 	public static void startScan() {
-		
+		status = true;
 	}
 	public static void stopScan() {
-		
+		status = false;
 	}
 	//Donne la valuer de status
 	public static boolean getStatus() {
@@ -23,13 +23,22 @@ public class Ultrason {
 	}
 	//Renvoie la distance mesurée par le capteur
 	public static float getDistance() {
-		float[] distance = new float[Capteur.ULTRASON.sampleSize()];
-		return distance[0];
+		float[] tabDistance = new float[Capteur.ULTRASON.sampleSize()];
+		Capteur.ULTRASON.fetchSample(tabDistance, 0);
+		distance = tabDistance[0];
+		return distance;
 	}
 	//Détecte la présence d'un autre robot
 	public static boolean autreRobot() {
-		boolean autreRobot=false;
-		return autreRobot;
+		bruitDetecte=false;
+		
+		float[] autreRobot = new float[Capteur.ECOUTE.sampleSize()];
+		Capteur.ECOUTE.fetchSample(autreRobot, 0);
+		
+		//ECOUTE renvoie 1 ou 0
+		if(autreRobot[0]==1) {bruitDetecte=true;}
+		
+		return bruitDetecte;
 	}
 	
 }
