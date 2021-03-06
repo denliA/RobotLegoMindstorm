4fb3936957190a6dd50db7881068edf98505bbb1
il07621 <il07621@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-03-06 11:36:04 +0000

    Création de la classe Pince

--- trunk/src/moteurs/Pince.java
+++ trunk/src/moteurs/Pince.java
@@ -1,5 +1,37 @@
 package moteurs;
 
 public class Pince {
-
+	
+	//Attribut
+	//les pinces s'ouvriront toujours de la même valeur par défaut
+	private static final float OUVERTURE;//TO DO 
+	private static boolean ouvert;
+	
+	//Méthodes
+	public static boolean getOuvert() {
+		return ouvert;
+	}
+	
+	//Ouverture et fermeture des pinces
+	//méthodes modifiant la valeur de ouvert simultanément
+	public static void ouvrir() {
+		if(ouvert==true) {
+			//TO DO			
+		}
+		else {
+			
+			ouvert = true;
+		}
+	}
+	public static void fermer() {
+		if(ouvert==false) {
+			//TO DO
+		}
+		else {
+			Moteur.MOTEUR_PINCE.setSpeed();
+			Moteur.MOTEUR_PINCE.backward();
+			Delay.msDelay();
+			ouvert = false;
+		}
+	}
 }
