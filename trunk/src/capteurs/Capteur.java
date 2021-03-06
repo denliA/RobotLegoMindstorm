4eab52c1406310e1c7425f49e9a7d087d787b597
il09104 <il09104@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-03-06 10:11:35 +0000

    Atributs des capteurs dans capteurs.Capteurs

--- trunk/src/capteurs/Capteur.java
+++ trunk/src/capteurs/Capteur.java
@@ -1,12 +1,20 @@
 package capteurs;
 
 import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.port.Port;
 import lejos.hardware.sensor.*;
+import lejos.robotics.SampleProvider;
 
 
 public class Capteur {
-	final private static EV3ColorSensor COLOR_SENSOR = LocalEV3.get().getPort("S3"); // Port de branchement du capteur de couleurs
+	final private static EV3ColorSensor COLOR_SENSOR = new EV3ColorSensor(LocalEV3.get().getPort("S3")); // Port de branchement du capteur de couleurs
+	final private static EV3TouchSensor TOUCH_SENSOR = new EV3TouchSensor(LocalEV3.get().getPort("S1"));
+	final private static EV3UltrasonicSensor ULTRASONIC_SENSOR = new EV3UltrasonicSensor(LocalEV3.get().getPort("S2"));
+	final public static SampleProvider TOUCHER = TOUCH_SENSOR.getTouchMode();
+	final public static SampleProvider RGB = COLOR_SENSOR.getRGBMode();
+	final public static SampleProvider LUMIERE_AMBIANTE = COLOR_SENSOR.getAmbientMode();
+	final public static SampleProvider ID_COULEUR = COLOR_SENSOR.getColorIDMode();
+	final public static SampleProvider ULTRASON = ULTRASONIC_SENSOR.getDistanceMode();
+	final public static SampleProvider ECOUTE = ULTRASONIC_SENSOR.getListenMode();
 	
 	
 	
