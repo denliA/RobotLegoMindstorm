140fa3b7e94bec69b67450fcd2ab2b8e2a5991bf
il09104 <il09104@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-03-17 12:20:11 +0000

    Correction de l'algorithme pour trouver la couleur (getCouleurLigne)

--- trunk/src/capteurs/Couleur.java
+++ trunk/src/capteurs/Couleur.java
@@ -3,7 +3,10 @@ package capteurs;
 import lejos.utility.Delay;
 import lejos.utility.Timer;
 import lejos.utility.TimerListener;
-import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+
 
 public class Couleur {
 	
@@ -14,12 +17,11 @@ public class Couleur {
 	private static float lumiere;
 	private static float IDCouleur;
 	private static float intensiteRouge;
-	
 	private static byte modeFlag;  //4bits 0000e3e2e1e0 avec e3 : light, e2 : RGB, e1 : ID, e0 : RedMode
-	private final static byte REDMODE = 0b1;
-	private final static byte IDMODE = 0b10;
-	private final static byte RGBMODE = 0b100;
-	private final static byte LIGHTMODE = 0b1000;
+	final static byte REDMODE = 0b1;
+	final static byte IDMODE = 0b10;
+	final static byte RGBMODE = 0b100;
+	final static byte LIGHTMODE = 0b1000;
 	
 	
 	private final static Object lock = new Object();
@@ -35,40 +37,7 @@ public class Couleur {
 	});
 	
 	
-	// énumération des couleurs, avec les bornes de celles-ci
-	enum CouleurLigne { 
-		ROUGE ( null, new float[] {2.80f, 3.80f, 0.45f, 0.60f, 0.10f, 0.20f}), 
-		VERTE (null, new float[] {0.30f, 0.40f, 0.20f, 0.25f, 0.58f, 0.70f}),
-		BLEUE (null, new float[] {0.17f, 0.27f, 0.61f, 0.78f, 2.50f, 4.00f}),
-		BLANCHEP (null, new float[] {0.64f, 0.73f, 0.53f, 0.63f, 0.77f, 0.95f}),
-		BLANCHEF (null, new float[] {0.65f, 0.76f, 0.52f, 0.65f, 0.75f, 0.92f}), 
-		NOIREH(new float[] {0,10,0,10,0,10 },null),
-		NOIREV(new float[] {0,10,0,10,0,10 },null),
-		NOIRE(new float[] {0,10,0,10,0,10 },null),
-		JAUNE (null, new float[] {0.75f, 0.83f, 0.15f, 0.20f, 0.18f, 0.26f}),
-		GRIS(null, null), //TODO
-		VIDE(new float[] {0,1,0,1,0,1 },null),
-		INCONNU(null, null);
-		
-		Intervalle IRGB;
-		Intervalle IRatios;
-		
-		
-		// bc : bornesCouleurs : {rouge_min, rouge_max, vert_min, vert_max, bleu_min, bleu_max} 
-		// br : bornesRatios : { R/G_min, R/G_max, B/G_min, B/G_max, B/R_min, B/R_max }
-		// pour des valeurs à ne pas prendre en compte, le min et le max de celles-ci doivent tous les deux être à Float.NaN
-		private CouleurLigne(float[] bc, float[] br) {
-			if (bc != null)
-				IRGB = new Intervalle(new float[] {bc[0], bc[2], bc[4]}, new float[] {bc[1], bc[3], bc[5]});
-			else
-				IRGB = null;
-			if (br != null)
-				IRatios = new Intervalle(new float[] {br[0], br[2], br[4]}, new float[] {br[1], br[3], br[5]});
-			else 
-				IRatios=null;
-		}
-		
-	}
+
 	
 	//Avoir la valeur de la couleur suivant l'enumeration de leJOS
 	public static float getColorID() {
@@ -125,17 +94,32 @@ public class Couleur {
 		synchronized(lock) {
 			float[] RGB = getRGB();
 			float[] ratios = getRatios();
+			ConcurrentHashMap <CouleurLigne, Float> candidats = new ConcurrentHashMap<>();
 			for (CouleurLigne couleur : CouleurLigne.values()) {
-				if (couleur.IRatios!=null) {
-					if (couleur.IRatios.contains(ratios))
-						return couleur;
-				}
-				else if (couleur.IRGB!=null) {
+				Float val = candidats.get(couleur);
+				if (couleur.IRGB!=null) {
 					if (couleur.IRGB.contains(RGB))
-						return couleur;
+						candidats.put(couleur, (val == null ? 1f : (val=val+1)));
+					else 
+						candidats.put(couleur, (val==null)? -.5f : (val=val-0.5f));
+				}
+				else if (couleur.IRatios!=null) {
+					if (couleur.IRatios.contains(ratios))
+						candidats.put(couleur, (val == null ? 1f : (val=val+1)));
+					else 
+						candidats.put(couleur, (val==null)? -.5f : (val=val-0.5f));
 				}
 			}
-			return CouleurLigne.INCONNU;		
+			System.out.println(candidats.toString());
+			CouleurLigne cand=null;
+			float max = 0;
+			for (CouleurLigne c : CouleurLigne.values()) {
+				if (candidats.get(c)!= null &&  candidats.get(c)>max) {
+					cand = c;
+					max = candidats.get(c);
+				}
+			}
+			return cand==null?  CouleurLigne.INCONNU : cand;
 		}
 		
 	}
@@ -155,9 +139,9 @@ public class Couleur {
 			if((modeFlag & RGBMODE) != 0) {
 				float[] couleurRGB = new float[Capteur.RGB.sampleSize()];
 				Capteur.RGB.fetchSample(couleurRGB, 0);
-				rouge = couleurRGB[0];
-				vert = couleurRGB[1];
-				bleu = couleurRGB[2];
+				rouge = 255*couleurRGB[0];
+				vert = 255*couleurRGB[1];
+				bleu = 255*couleurRGB[2];
 			}
 			Delay.msDelay(delai);
 			if((modeFlag & IDMODE)!=0) {
@@ -175,31 +159,3 @@ public class Couleur {
 	}
 }
 
-
-class Intervalle {
-	float[] min;
-	float[] max;
-	
-	public Intervalle(float min, float max) {
-		this(new float[] {min}, new float[] {max});
-	}
-	public Intervalle(float[] min, float[] max) {
-		assert(min.length == max.length);
-		this.min = min;
-		this.max = max;
-	}
-	
-	public boolean contains(float[] values) {
-		return contains(values, 0, min.length);
-	}
-	
-	public boolean contains(float[] values, int beg, int end) {
-		assert(values.length == end-beg);
-		for (int i=0; i<end-beg; i++)
-			if (values[i] > max[i] || values[i] < min[i])
-				return false;
-		return true;
-		
-	}
-	
-}
