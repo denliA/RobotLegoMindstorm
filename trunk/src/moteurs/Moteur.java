bc80e760445a65391df392435a47f8d61d522eff
il07621 <il07621@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-03-18 14:35:22 +0000

    Petite correction de Moteurs

--- trunk/src/moteurs/Moteur.java
+++ trunk/src/moteurs/Moteur.java
@@ -6,14 +6,11 @@ import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
 
 public class Moteur {
-	/* Ca ouvre le moteur? Pas besoin de ouvrirMoteurs()? */
+	//ouvre les moteurs qui sont réutilisables dans les autres classes
 	public static EV3LargeRegulatedMotor MOTEUR_GAUCHE = new EV3LargeRegulatedMotor(MotorPort.B);
 	public static EV3LargeRegulatedMotor MOTEUR_DROIT = new EV3LargeRegulatedMotor(MotorPort.A);
 	public static EV3MediumRegulatedMotor MOTEUR_PINCE = new EV3MediumRegulatedMotor(MotorPort.C);
 	
-	public static void ouvrirMoteurs() {
-		//TO DO
-	}
 	public static void fermerMoteurs() {
 		MOTEUR_GAUCHE.close();
 		MOTEUR_DROIT.close();
