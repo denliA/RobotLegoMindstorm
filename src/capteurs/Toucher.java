d4d7e6afdeaf438931eeacd55c4e63b129e48136
il07373 <71606408+UnHommeLambda@users.noreply.github.com>
2021-03-20 09:57:08 +0000

    Création d'un test pour le capteur de contact.

--- src/capteurs/Toucher.java
+++ src/capteurs/Toucher.java
@@ -1,36 +1,62 @@
 package capteurs;
 
-public class Toucher implements Runnable{
+import lejos.hardware.Button;
+import lejos.utility.Delay;
+import lejos.utility.Timer;
+import lejos.utility.TimerListener;
+
+public class Toucher{
 	
 	//Atrributs de la classe Toucher
 	private static boolean status;
 	private static boolean touche;
 	
-	//Detection de l'activation du capteur de contact
-	public static boolean detecterPalet() {
-		float[] touched = new float[Capteur.TOUCHER.sampleSize()];
-		Capteur.TOUCHER.fetchSample(touched, 0);
-		touche = (touched[0]==1);
+	//Pour lancer des analyses de manière périodique
+	private static Timer lanceur = new Timer(100, 
+			new TimerListener() {
+		public void timedOut() {
+				setTouche();
+		}
+	});
+	
+	public static boolean getTouche() {
 		return(touche);
 	}
 	
-	//Pour implementer Runnable
-	public void run() {
-		detecterPalet();
+	public static void setTouche() {
+		float[] touched = new float[Capteur.TOUCHER.sampleSize()];
+		Capteur.TOUCHER.fetchSample(touched, 0);
+		touche = (touched[0]==1);
+		
 	}
 	
 	//Pour activer la lecture de la valeur envoyée par le scanner
-	public void startScan() {
+	public static void startScan() {
+		lanceur.setDelay(50);
+		lanceur.start();
 		status = true;
 	}
 	
 	//Pour désactiver la lecture de la valeur envoyée par le scanner
-	public void stopScan() {
+	public static void stopScan() {
+		lanceur.stop();
 		status = false;
 	}
 	
 	//Retourne le status pour savoir l'état actuel
-	public boolean getStatus() {
+	public static boolean getStatus() {
 		return(status);
+	}
+}
+
+class testToucher{
+	public static void main(String[] args) {
+		Toucher.startScan();
+		new Capteur();
+		while(Button.ENTER.isUp()) {
+			System.out.println(Toucher.getTouche());
+			Delay.msDelay(500);
+		}
+		Toucher.stopScan();
 	}
 }
