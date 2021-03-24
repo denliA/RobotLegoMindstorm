853ae0b5f9e589e36f3ced0912bf33e41fc09e89
il07621 <il07621@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-03-24 18:05:49 +0000

    Amélioration de la classe Capteur et ajout de commentaires.

--- trunk/src/capteurs/Capteur.java
+++ trunk/src/capteurs/Capteur.java
@@ -6,14 +6,33 @@ import lejos.robotics.SampleProvider;
 
 
 public class Capteur {
+	
+	//on initialise les objets Sensor de leJos en precisant le port de branchement
+	//ouverture des capteurs
 	final private static EV3ColorSensor COLOR_SENSOR = new EV3ColorSensor(LocalEV3.get().getPort("S3")); // Port de branchement du capteur de couleurs
 	final private static EV3TouchSensor TOUCH_SENSOR = new EV3TouchSensor(LocalEV3.get().getPort("S1"));
 	final private static EV3UltrasonicSensor ULTRASONIC_SENSOR = new EV3UltrasonicSensor(LocalEV3.get().getPort("S2"));
+	
+	/*Creation et initialisation des objets de type SampleProvider.
+	Ils permettent la recuperation des donnees des capteurs.
+	Ces objets sont utilises par les autres classes du package pour obtenir les valeurs mesurees*/
+	
+	//Capteur toucher pour la classe Toucher
 	final public static SampleProvider TOUCHER = TOUCH_SENSOR.getTouchMode();
+	
+	//sample providers du capteur de couleur pour la classe Couleur
 	final public static SampleProvider RGB = COLOR_SENSOR.getRGBMode();
 	final public static SampleProvider LUMIERE_AMBIANTE = COLOR_SENSOR.getAmbientMode();
 	final public static SampleProvider ID_COULEUR = COLOR_SENSOR.getColorIDMode();
 	final public static SampleProvider ROUGE = COLOR_SENSOR.getRedMode();
+	
+	//sample providers pour la classe Ultrason
 	final public static SampleProvider ULTRASON = ULTRASONIC_SENSOR.getDistanceMode();
 	final public static SampleProvider ECOUTE = ULTRASONIC_SENSOR.getListenMode();
+	//Methode pour fermer les capteurs ouverts au debut de la classe. 
+	public static void fermerCapteurs() {
+		COLOR_SENSOR.close();
+		TOUCH_SENSOR.close();
+		ULTRASONIC_SENSOR.close();
+	}
 }
