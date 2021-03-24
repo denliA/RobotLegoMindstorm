714941948232a114ee51b9d83f23c3677f71e780
il07621 <il07621@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-03-24 18:37:17 +0000

    Mise en forme et ajout de commentaires à la classe Toucher

--- trunk/src/capteurs/Toucher.java
+++ trunk/src/capteurs/Toucher.java
@@ -8,23 +8,31 @@ import lejos.utility.TimerListener;
 public class Toucher{
 	
 	//Atrributs de la classe Toucher
+	//status indique si la capteur est actif
 	private static boolean status;
+	//touche contient True si le capteur est activé par un palet
 	private static boolean touche;
 	
-	//Pour lancer des analyses de manière périodique
-	private static Timer lanceur = new Timer(100, 
-			new TimerListener() {
-		public void timedOut() {
-				setTouche();
+	//Pour lancer des analyses de maniere periodique et eviter l'activation permanente du capteur
+	private static Timer lanceur = 
+		new Timer(100, new TimerListener() {
+			//au bout du temps imparti (100 ms)
+				public void timedOut() {
+					//appel à la foction qui scanne effectivement
+					setTouche();
+				}
 		}
-	});
+	);
 	
 	public static boolean getTouche() {
 		return(touche);
 	}
 	
+	//change la valeur de touche en fonction du scan
 	public static void setTouche() {
+		//creation du tableau recueillant les données du sampler
 		float[] touched = new float[Capteur.TOUCHER.sampleSize()];
+		//récupération des données
 		Capteur.TOUCHER.fetchSample(touched, 0);
 		touche = (touched[0]==1);
 		
@@ -32,6 +40,7 @@ public class Toucher{
 	
 	//Pour activer la lecture de la valeur envoyée par le scanner
 	public static void startScan() {
+		//le temps passe à 50 ms entre chaque scan
 		lanceur.setDelay(50);
 		lanceur.start();
 		status = true;
