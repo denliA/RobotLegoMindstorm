693d47641caa866d1633d1e445df9cbe0a185683
il07621 <91056792+LaSeuleEtParfaiteMB@users.noreply.github.com>
2021-03-31 09:59:51 +0000

    Création de la classe de test pour le capteur ultrason

---
+++ src/capteurs/TestUltrason.java
@@ -0,0 +1,27 @@
+package capteurs;
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+
+public class TestUltrason {
+	public static void main(String[] args) {
+		//initialisation : création des sampler de la classe Capteur
+		new Capteur();
+		Ultrason.startScan();
+		
+		//nettoyage de l'ecran du robot afin d'afficher les messages sans parasite
+		LCD.clear();
+		
+		while(Button.ENTER.isUp()) {
+			float d;
+			Ultrason.setDistance();
+			d = Ultrason.getDistance();
+			LCD.drawString("distance : "+d, 0, 0);
+			
+			Ultrason.setBruitDetecte();
+			boolean b = Ultrason.getBruitDetecte();
+			if(b) LCD.drawString("Il y a un robot", 0, 2);
+			else LCD.drawString("Y a pas de robot", 0, 2);
+		}
+	}
+}
