e3291d75f73790e30c2ca0355e86ba6a5fc80ceb
il09104 <30708312+DaBlumer@users.noreply.github.com>
2021-04-01 00:09:48 +0000

    Rajout des intersections pour les couleurs, ainsi qu'une fonctionnalité d'historique pour les 15 dernières couleurs trouvées

--- src/capteurs/Couleur.java
+++ src/capteurs/Couleur.java
@@ -29,12 +29,15 @@ public class Couleur {
 	private static float lumiere;
 	private static float IDCouleur;
 	private static float intensiteRouge;
+	private static CouleurLigne[] buffer = new CouleurLigne[15];
+	private static int bufferIndex = 0;
 	
 	/**
 	 * indicateur permettant de décider quelles mesures prendre et lesquelles ignorer.
 	 * <p>
-	 * Les trois 4 bits inférieurs e3e2e1e0 sont utilisés:
+	 * Les 5 bits inférieurs e4e3e2e1e0 sont utilisés:
 	 * <ul> 
+	 * <li> e4 pour activer l'historique des valeurs
 	 * <li> e3 pour la lumière ambiante
 	 * <li> e2 pour le mode RGB
 	 * <li> e1 pour le ColorID (algorithme par défaut de Lego)
@@ -47,22 +50,21 @@ public class Couleur {
 	final static public byte IDMODE = 0b10;
 	final static public byte RGBMODE = 0b100;
 	final static public byte LIGHTMODE = 0b1000;
+	final static public byte BUFFERING = 0b10000;
 	
 	// Assure la synchronisation des opérations : si une mesure est en train d'être faite, on ne veut pas accéder aux vairables modifées par celle-ci  entre temps.
 	private final static Object lock = new Object();
+	private final static Object buffer_lock = new Object();
 	
 	// Pour lancer périodiquement des mesures
 	private static Timer lanceur = new Timer(100, 
 			new TimerListener() {
 		public void timedOut() {
-			synchronized(lock) {
-				try{
-				update();
-				}catch(IllegalArgumentException e){
-					Sound.beepSequenceUp();
-					System.err.println(e.toString());
-					e.printStackTrace(System.err);
-					Capteur.ouvrirCapteurCouleur();
+			update();
+			if ((modeFlag&BUFFERING)==1) {
+				CouleurLigne c = getCouleurLigne();
+				synchronized(buffer_lock) {
+					buffer[bufferIndex=((bufferIndex+1)%buffer.length)] = c;
 				}
 			}
 		}
@@ -148,12 +150,13 @@ public class Couleur {
 	 * @return la couleur trouvée par l'algorithme, ou CouleurLigne.INCONNUE sinon.
 	 */
 	public static CouleurLigne getCouleurLigne() {
+		float[] RGB, ratios;
 		setScanMode((byte) (getScanMode() | RGBMODE)); // On s'assure que l'on scanne les valeurs RGB 
 		Delay.msDelay(10);
 		synchronized(lock) {
-			float[] RGB = getRGB();
-			float[] ratios = getRatios();
-			
+			RGB = getRGB();
+			ratios = getRatios();
+		}
 			// On associe pour chaque couleur rencontrée une valeur flottante, représentant grossièrement la probabilité que cette couleur soit la bonne.
 			ConcurrentHashMap <CouleurLigne, Float> candidats = new ConcurrentHashMap<>();
 			
@@ -161,7 +164,7 @@ public class Couleur {
 			 * Pour chaque couleur, on vérifie si elle définit un intervalle de valeurs directes (resp de rapports),
 			 * et si c'est le cas on regarde si notre mesure appartient à cet intervalle.
 			 */
-			for (CouleurLigne couleur : CouleurLigne.values()) {
+			for (CouleurLigne couleur : CouleurLigne.principales) {
 				Float val = candidats.get(couleur);
 				if (couleur.IRGB!=null) {
 					if (couleur.IRGB.contains(RGB))
@@ -192,10 +195,61 @@ public class Couleur {
 			}
 			
 			// On retourne la couleur trouvée, ou CouleurLigne.INCONNU si il n'y a aucun candidat.
-			System.out.println(candidats);
+			//System.out.println(candidats);
+			//return cand==null?  CouleurLigne.INCONNU : cand;
+			if (cand ==null)
+				return cand;
+			candidats.clear();
+			for(CouleurLigne c : CouleurLigne.values()) {
+				for(CouleurLigne intersect : c.intersetions) {
+					Float val = candidats.get(intersect);
+					if (intersect.IRGB!=null) {
+						if (intersect.IRGB.contains(RGB))
+							candidats.put(intersect, (val == null ? (val=intersect.pos_confiance_IRGB) : (val=val+intersect.pos_confiance_IRGB)));
+						else 
+							candidats.put(intersect, (val==null)? (val=intersect.neg_confiance_IRGB) : (val=val+intersect.neg_confiance_IRGB));
+						//if (intersect==intersectLigne.BLANCHEF) System.out.println(intersect+" IRGB "+intersect.pos_confiance_IRGB+" "+intersect.neg_confiance_IRGB + " " + candidats.get(intersect));
+					}
+					if (intersect.IRatios!=null) {
+						if (intersect.IRatios.contains(ratios))
+							candidats.put(intersect, (val == null ? (val=intersect.pos_confiance_IRatios) : (val=val+intersect.pos_confiance_IRatios)));
+						else 
+							candidats.put(intersect, (val==null)? (val=intersect.neg_confiance_IRatios) : (val=val+intersect.neg_confiance_IRatios));
+						//System.out.println(intersect+" IRatios "+intersect.pos_confiance_IRatios+" "+intersect.neg_confiance_IRatios);
+					}
+				}
+			}
+			cand=null;
+			max = 0;
+			for (CouleurLigne c : CouleurLigne.values()) {
+				if (candidats.get(c)!= null &&  candidats.get(c)>max) {
+					cand = c;
+					max = candidats.get(c);
+				}
+			}
 			return cand==null?  CouleurLigne.INCONNU : cand;
-		}
 		
+	}
+	
+	
+	public static CouleurLigne getLastCouleur() {
+		synchronized(buffer_lock) {
+			return buffer[bufferIndex];
+		}
+	}
+	
+	public static CouleurLigne[] historiqueCouleurs(int nb) {
+		CouleurLigne[] hist = new CouleurLigne[nb];
+		int n = 0;
+		synchronized(buffer) {
+			for (int i=bufferIndex; i>=0&&n<nb; i--, n++) {
+				hist[n] = buffer[i];
+			}
+			for (int i=14; i>bufferIndex&&n<nb; i--, n++) {
+				hist[n] = buffer[i];
+			}
+		}
+		return hist;
 	}
 	
 	
@@ -211,32 +265,34 @@ public class Couleur {
 	 * @see lejos.robotics.SampleProvider
 	 */
 	private static void update(int delai) {
-		synchronized (lock) {
-			if((modeFlag & LIGHTMODE)!=0) {
-				float[] ambiantLight = new float [Capteur.LUMIERE_AMBIANTE.sampleSize()];
-				Capteur.LUMIERE_AMBIANTE.fetchSample(ambiantLight, 0);
-				lumiere = ambiantLight[0];
-			}
-			Delay.msDelay(delai);
-			if((modeFlag & RGBMODE) != 0) {
-				float[] couleurRGB = new float[Capteur.RGB.sampleSize()];
-				Capteur.RGB.fetchSample(couleurRGB, 0);
+		if((modeFlag & LIGHTMODE)!=0) {
+			float[] ambiantLight = new float [Capteur.LUMIERE_AMBIANTE.sampleSize()];
+			Capteur.LUMIERE_AMBIANTE.fetchSample(ambiantLight, 0);
+			synchronized(lock) { lumiere = ambiantLight[0]; }
+		}
+		Delay.msDelay(delai);
+		if((modeFlag & RGBMODE) != 0) {
+			float[] couleurRGB = new float[Capteur.RGB.sampleSize()];
+			Capteur.RGB.fetchSample(couleurRGB, 0);
+			synchronized(lock) {
 				rouge = 255*couleurRGB[0];
 				vert = 255*couleurRGB[1];
 				bleu = 255*couleurRGB[2];
 			}
-			Delay.msDelay(delai);
-			if((modeFlag & IDMODE)!=0) {
-				float[] couleur_id = new float[Capteur.ID_COULEUR.sampleSize()];
-				Capteur.ID_COULEUR.fetchSample(couleur_id, 0);
+		}
+		Delay.msDelay(delai);
+		if((modeFlag & IDMODE)!=0) {
+			float[] couleur_id = new float[Capteur.ID_COULEUR.sampleSize()];
+			Capteur.ID_COULEUR.fetchSample(couleur_id, 0);
+			synchronized(lock) {
 				IDCouleur = couleur_id[0];
 			}
-			Delay.msDelay(delai);
-			if((modeFlag & REDMODE)!=0) {
-				float[] rouge = new float[1];
-				Capteur.ROUGE.fetchSample(rouge, 0);
-				intensiteRouge = rouge[0];
-			}
+		}
+		Delay.msDelay(delai);
+		if((modeFlag & REDMODE)!=0) {
+			float[] rouge = new float[1];
+			Capteur.ROUGE.fetchSample(rouge, 0);
+			synchronized(lock) {intensiteRouge = rouge[0];}
 		}
 	}
 }
