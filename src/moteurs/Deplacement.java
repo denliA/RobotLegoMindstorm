8a7560b9f7c273bd57d1bbb216de5646e65a4770
il09104 <30708312+DaBlumer@users.noreply.github.com>
2021-04-01 00:29:43 +0000

    Fin de l'implémentation des classes abstraites ConditionArret et Deplacement qui pourraient simplifier les déplacements condiionnels qui doivent s'arrêter selon une condition

--- src/moteurs/Deplacement.java
+++ src/moteurs/Deplacement.java
@@ -1,21 +1,35 @@
 package moteurs;
 
+import lejos.utility.Timer;
+import lejos.utility.TimerListener;
+
 public abstract class Deplacement extends Thread {
 	
 	public enum TypeDeplacement {EXCLUSIF, DEMON, AIDE}
-	public enum StatusDeplacement { PRET, ENCOURS, ENATTENTE, FINI }
+	public enum StatusDeplacement { PRET, ENCOURS, ENATTENTE, INTERROMPU, FINI }
 	
 	protected ConditionArret condition;
 	protected  StatusDeplacement status;
 	protected TypeDeplacement type;
-	Thread verificateur;
+	Timer verificateur;
 	
 	public Deplacement(ConditionArret condition, TypeDeplacement type) {
-		
+		this.condition = condition;
+		this.type =  type;
+		this.status = StatusDeplacement.PRET;
+		verificateur = new Timer(10, new TimerListener() {
+			public void timedOut() {
+				if(Deplacement.this.condition.eval()) {
+					status = StatusDeplacement.FINI;
+					interrupt();
+				}
+			}
+		});
 	}
 	
 	public void lancer() {
-		
+		this.status = StatusDeplacement.ENCOURS;
+		run();
 	};
 	
 	public StatusDeplacement getStatus() {
@@ -26,6 +40,10 @@ public abstract class Deplacement extends Thread {
 		return condition.getCausesArret();
 	}
 	
+	public void Arreter() {
+		this.status = StatusDeplacement.INTERROMPU;
+		super.interrupt();
+	}
 	
 	
 }
