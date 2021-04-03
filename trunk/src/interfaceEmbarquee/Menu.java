0015905d286600d75dcb129a04e3c62fd7c5f5fc
ij07411 <ij07411@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-04-03 22:29:23 +0000

    interface embarquée uniquement textuelle

--- trunk/src/interfaceEmbarquee/Menu.java
+++ trunk/src/interfaceEmbarquee/Menu.java
@@ -3,105 +3,40 @@ package interfaceEmbarquee;
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 
-public class Menu {
+public class Menu implements Lancable{
+	String titre;
+	Lancable[] tab;
 	
-	public static void menuPrincipal() {
-		LCD.clear();
-		LCD.drawString("Pince", 7, 1);
-		int button = -1;
-		//int rotate = 0;
-		int choix = 1;
-		while(button != Button.ID_ESCAPE) {
-			LCD.clear(1,2, 100);
-			//Delay.msDelay(200);
-			LCD.drawString("->", 1, choix+1);
-			LCD.drawString("Mode solo", 3, 2);
-			LCD.drawString("Mode competition", 3, 3);
-			button = Button.waitForAnyPress();
-			if (button == Button.ID_DOWN) {
-				choix = (choix%5)+1;
-				//lejos.hardware.Sound.beep();
-			}
-			else if (button == Button.ID_UP)
-				choix = (choix == 1) ? 5 :(choix-1);
-			else if (button == Button.ID_ENTER) {
-				switch (choix) {
-				case 1:
-					modeSolo();
-					break;
-				case 2:
-					modeCompetition();
-					break;
-				}
-			}
-			Button.waitForAnyEvent();
-		}
-		
+	public Menu(Lancable tab[]) {
+		this.tab = tab;
 	}
 	
-	public static void modeSolo() {
-		LCD.clear();
-		LCD.drawString("Pince", 7, 1);
-		int button = -1;
-		//int rotate = 0;
-		int choix = 1;
-		while(button != Button.ID_ESCAPE) {
-			LCD.clear(1,2, 100);
-			//Delay.msDelay(200);
-			LCD.drawString("->", 1, choix+1);
-			LCD.drawString("Mode solo", 3, 2);
-			LCD.drawString("Mode competition", 3, 3);
-			button = Button.waitForAnyPress();
-			if (button == Button.ID_DOWN) {
-				choix = (choix%5)+1;
-				//lejos.hardware.Sound.beep();
-			}
-			else if (button == Button.ID_UP)
-				choix = (choix == 1) ? 5 :(choix-1);
-			else if (button == Button.ID_ENTER) {
-				switch (choix) {
-				case 1:
-					modeSolo();
-					break;
-				case 2:
-					modeCompetition();
-					break;
-				}
-			}
-			Button.waitForAnyEvent();
-		}
+	public String getTitre() {
+		return titre;
 	}
 	
-	public static void modeCompetition() {
+	public void lancer() {
 		LCD.clear();
-		LCD.drawString("Pince", 7, 1);
+		LCD.drawString(titre, 7, 1);
 		int button = -1;
-		//int rotate = 0;
 		int choix = 1;
 		while(button != Button.ID_ESCAPE) {
 			LCD.clear(1,2, 100);
-			//Delay.msDelay(200);
 			LCD.drawString("->", 1, choix+1);
-			LCD.drawString("Mode solo", 3, 2);
-			LCD.drawString("Mode competition", 3, 3);
+			for (int i=0;i<tab.length;i++) {
+				LCD.drawString(tab[i].getTitre(), 3, i+1);
+			}
 			button = Button.waitForAnyPress();
 			if (button == Button.ID_DOWN) {
-				choix = (choix%5)+1;
+				choix = (choix%tab.length)+1;
 				//lejos.hardware.Sound.beep();
 			}
 			else if (button == Button.ID_UP)
-				choix = (choix == 1) ? 5 :(choix-1);
+				choix = (choix == 1) ? tab.length :(choix-1);
 			else if (button == Button.ID_ENTER) {
-				switch (choix) {
-				case 1:
-					modeSolo();
-					break;
-				case 2:
-					modeCompetition();
-					break;
-				}
+				tab[choix].lancer();
 			}
 			Button.waitForAnyEvent();
-		}
+		}	
 	}
 }
