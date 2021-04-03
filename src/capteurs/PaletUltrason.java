e2c5c9866c41c13b1e5dc14a649db23da3ed382a
il07621 <91056792+LaSeuleEtParfaiteMB@users.noreply.github.com>
2021-04-03 11:52:48 +0000

    Le robot capte le palet, se dirige vers lui et, parfois, l'attrape. Il faut encore calibrer pour que le robot se dirige correctement vers le palet.

--- src/capteurs/PaletUltrason.java
+++ src/capteurs/PaletUltrason.java
@@ -1,12 +1,17 @@
 package capteurs;
 
 import exceptions.OuvertureException;
+import lejos.utility.Delay;
+import moteurs.Pince;
+import moteurs.MouvementsBasiques;
+import lejos.hardware.Button;
 
 public class PaletUltrason {
+	
 	public static void main(String[] args) throws OuvertureException {
 		//Lancer les capteurs
 		new Capteur();
-		Ultrason.startScan();
+		//Ultrason.startScan();
 		Toucher.startScan();
 		//initialiser l'infini
 		float infini = Float.POSITIVE_INFINITY;
@@ -14,22 +19,66 @@ public class PaletUltrason {
 		Ultrason.setDistance();
 		float d = Ultrason.getDistance();
 		
-		if(d<infini&&!Toucher.getTouche()) {//on voit le palet sans le toucher
-			moteurs.MouvementsBasiques.avancer();
+		System.out.println("Premiere mesure de d : "+d);
+		
+		//ouverture des pinces
+		if(!Pince.getOuvert()) {
+			Pince.ouvrir();
 		}
-		else {
-			if(Toucher.getTouche()) {
-				moteurs.MouvementsBasiques.arreter();
-				moteurs.Pince.fermer();
+		
+		//tant qu'on n'a pas attrape le palet
+		while(Pince.getOuvert()&&Button.ENTER.isUp()) {
+			//se mettre dans la direction du palet en evitant de capter le decor
+			int angle = 0;
+			while((d>2||d<=0)&&angle<360) {
+				angle+=10;
+				MouvementsBasiques.tourner(10);
+				Ultrason.setDistance();
+				d = Ultrason.getDistance();
+				System.out.println(d);
 			}
-			else {
-				int angle = 0;
-				while(d==infini&&!Toucher.getTouche()&&angle<360) {
-					angle+=10;
-					moteurs.MouvementsBasiques.tourner(10);
-				}
+			if(angle>=354) {
+				break;
 			}
+			
+			//on sauvegarde la distance de depart une fois le palet repere
+			float dDepart = Ultrason.getDistance();
+			System.out.println("dDepart vaut : "+dDepart);
+			
+			//on avance tant qu'on ne depasse pas le palet
+			MouvementsBasiques.avancer();
+			while(d<=(dDepart+0.5)&&!Toucher.getTouche()) {
+				Ultrason.setDistance();
+				d = Ultrason.getDistance();
+				System.out.println(d);
+			}
+			MouvementsBasiques.arreter();
+			//si on le touche, on attrappe
+			
+			Pince.fermer();
+				
 		}
+		
+//		//avancer vers le palet tant qu'on le capte
+//		moteurs.MouvementsBasiques.avancer();
+//		while(((d=Ultrason.getDistance())>0&&d!=infini)&&!Toucher.getTouche()) {
+//			System.out.println(d);
+//		}
+//		moteurs.MouvementsBasiques.arreter();
+//		
+//		//selon s'il est en contact avec le palet
+//		if(Toucher.getTouche()) {
+//			Pince.fermer();
+//		}
+//		else {
+//			int angle = 0;
+//			while(((d=Ultrason.getDistance())==infini||d<=0)&&!Toucher.getTouche()&&angle<360) {
+//				angle+=10;
+//				moteurs.MouvementsBasiques.tourner(10);
+//				System.out.println(d);
+//				}
+//			}
+		
 	}
 	
 }
