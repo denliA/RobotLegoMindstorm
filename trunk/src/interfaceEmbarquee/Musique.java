76877871328718dd186187ea69b3f14f39741995
ij07411 <ij07411@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-04-11 22:58:06 +0000

    Musique qui s'arrete grace a un executor et ModeCompetition

--- trunk/src/interfaceEmbarquee/Musique.java
+++ trunk/src/interfaceEmbarquee/Musique.java
@@ -1,62 +1,82 @@
 package interfaceEmbarquee;
 
 import java.io.File;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 
 public class Musique{
-    private static volatile boolean running = true;
+	static ExecutorService executor = Executors.newSingleThreadExecutor();
+	static Future<Void> future;
+	
+	
     
-    public static boolean getRunning() {
-    	return running;
-    }
-
-    public static void setRunning(boolean b) {
-    	running = b;
+    public static void startMusic(final String nameConfig,final String nameFile) throws InterruptedException{
+    	future = executor.submit(new Callable<Void>() {
+    		public Void call() {
+                LCD.clear();
+                int res;
+    			File fichier=null;
+    			if(Configurations.musique.getVal().equals(nameConfig)) {
+    				fichier = new File(nameFile);
+    				if (fichier==null) {
+    					Sound.beep();
+    					LCD.drawString("Fichier pas ouvert", 3, 2);
+    					System.out.println("Fichier pas ouvert");
+    				}
+    				res = Sound.playSample(fichier, Sound.VOL_MAX); //attention : instruction bloquante
+    				if (res<0) {
+    					System.out.println("fichier.waw au mauvais format");
+    					LCD.drawString("fichier.waw au mauvais format", 3, 4);
+    				}
+    			}
+    			else {
+    				System.out.println(nameConfig+" n'est pas la configuration choisie");
+    			}
+				return null;
+            }
+    	});
     }
     
     public static void stopMusic() {
-        running = false;
+        future.cancel(true); //va tenter d'arreter le thread en cours d'execution
     }
-    
-    public static void startMusic(final String nameConfig,final String nameFile) throws InterruptedException{
-    	Thread t = new Thread(new Runnable(){
-            public void run() {
-                while(!Thread.currentThread().isInterrupted()){
-                	LCD.clear();
-                	int res;
-    				File fichier=null;
-    				if(Configurations.musique.getVal().equals(nameConfig)) {
-    					fichier = new File(nameFile);
-    					if (fichier==null) {
-    						Sound.beep();
-    						LCD.drawString("Fichier pas ouvert", 3, 2);
-    						System.out.println("Fichier pas ouvert");
-    						return; //on sort de la fonction si le fichier n'est pas trouve
-    					}
-    					res = Sound.playSample(fichier, Sound.VOL_MAX);
-    					if (res<0)
-    						System.out.println("fichier.waw au mauvais format");
-    						LCD.drawString("fichier.waw au mauvais format", 3, 4);
-    					
-    				}
-    				else {
-    					System.out.println(nameConfig+" n'est pas la configuration choisie");
-    				}
-    				running=false; //la musique s'est jouée jusqu'au bout
-                }   
-            }});
     	
-    	t.start();
-        running=true;
-        while(running);//j'attends la fin de l'execution
-     
-        // Sleep a second, and then interrupt
-        try {
-            Thread.sleep(1000); //seuls les threads en etat de sleep ou de wait arretent immediatement leur executions si interrompus
-        } catch (InterruptedException e) {}
-        t.interrupt();
-    }
+//    	public static void startMusic(final String nameConfig,final String nameFile) throws InterruptedException{
+//    		executor.execute(new Runnable() {
+//    		public void run() {
+//                while(!executor.isShutdown()){
+//                	LCD.clear();
+//                	int res;
+//    				File fichier=null;
+//    				if(Configurations.musique.getVal().equals(nameConfig)) {
+//    					fichier = new File(nameFile);
+//    					if (fichier==null) {
+//    						Sound.beep();
+//    						LCD.drawString("Fichier pas ouvert", 3, 2);
+//    						System.out.println("Fichier pas ouvert");
+//    						return; //on sort de la fonction si le fichier n'est pas trouve
+//    					}
+//    					res = Sound.playSample(fichier, Sound.VOL_MAX); //attention : instruction bloquante
+//    					if (res<0)
+//    						System.out.println("fichier.waw au mauvais format");
+//    						LCD.drawString("fichier.waw au mauvais format", 3, 4);
+//    				}
+//    				else {
+//    					System.out.println(nameConfig+" n'est pas la configuration choisie");
+//    				}
+//                }
+//    		}
+//    	});
+//    }
+//    	
+//      public static void stopMusic() {
+//    	  executor.shutdownNow(); //va tenter d'arreter le thread en cours d'execution
+//      }
+      
 }
