7effd907141f6b56bbb7c34e37c8039615202426
ij07411 <ij07411@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-04-13 20:27:00 +0000

    Mode competition qui s'appuie sur le modeSolo

--- trunk/src/modeCompetition/TestModeCompetition.java
+++ trunk/src/modeCompetition/TestModeCompetition.java
@@ -1,9 +1,23 @@
 package modeCompetition;
 
+import exceptions.EchecGarageException;
+import exceptions.OuvertureException;
+
 public class TestModeCompetition {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			ModeCompetition.ramasserPalet(9,true);
+		} catch (EchecGarageException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OuvertureException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
