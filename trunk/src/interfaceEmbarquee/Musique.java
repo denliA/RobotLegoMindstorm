63cc429627b0ec2c2fa2e8e07652f3233c89a690
ij07411 <ij07411@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-04-13 13:21:43 +0000

    Interface ou la musique s'arrete enfin avec un thread

--- trunk/src/interfaceEmbarquee/Musique.java
+++ trunk/src/interfaceEmbarquee/Musique.java
@@ -9,74 +9,45 @@ import java.util.concurrent.Future;
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
 
 public class Musique{
 	static ExecutorService executor = Executors.newSingleThreadExecutor();
 	static Future<Void> future;
 	
+	static Thread t;
 	
-    
-    public static void startMusic(final String nameConfig,final String nameFile) throws InterruptedException{
-    	future = executor.submit(new Callable<Void>() {
-    		public Void call() {
-                LCD.clear();
-                int res;
-    			File fichier=null;
-    			if(Configurations.musique.getVal().equals(nameConfig)) {
-    				fichier = new File(nameFile);
-    				if (fichier==null) {
-    					Sound.beep();
-    					LCD.drawString("Fichier pas ouvert", 3, 2);
-    					System.out.println("Fichier pas ouvert");
-    				}
-    				res = Sound.playSample(fichier, Sound.VOL_MAX); //attention : instruction bloquante
-    				if (res<0) {
-    					System.out.println("fichier.waw au mauvais format");
-    					LCD.drawString("fichier.waw au mauvais format", 3, 4);
-    				}
-    			}
-    			else {
-    				System.out.println(nameConfig+" n'est pas la configuration choisie");
-    			}
-				return null;
-            }
-    	});
-    }
-    
-    public static void stopMusic() {
-        future.cancel(true); //va tenter d'arreter le thread en cours d'execution
-    }
-    	
-//    	public static void startMusic(final String nameConfig,final String nameFile) throws InterruptedException{
-//    		executor.execute(new Runnable() {
-//    		public void run() {
-//                while(!executor.isShutdown()){
-//                	LCD.clear();
-//                	int res;
-//    				File fichier=null;
-//    				if(Configurations.musique.getVal().equals(nameConfig)) {
-//    					fichier = new File(nameFile);
-//    					if (fichier==null) {
-//    						Sound.beep();
-//    						LCD.drawString("Fichier pas ouvert", 3, 2);
-//    						System.out.println("Fichier pas ouvert");
-//    						return; //on sort de la fonction si le fichier n'est pas trouve
-//    					}
-//    					res = Sound.playSample(fichier, Sound.VOL_MAX); //attention : instruction bloquante
-//    					if (res<0)
-//    						System.out.println("fichier.waw au mauvais format");
-//    						LCD.drawString("fichier.waw au mauvais format", 3, 4);
-//    				}
-//    				else {
-//    					System.out.println(nameConfig+" n'est pas la configuration choisie");
-//    				}
-//                }
-//    		}
-//    	});
-//    }
-//    	
-//      public static void stopMusic() {
-//    	  executor.shutdownNow(); //va tenter d'arreter le thread en cours d'execution
-//      }
-      
+	public static void startMusic(final String nameConfig,final String nameFile) throws InterruptedException{
+		t = new Thread(new Runnable() {
+			public void run() {
+	            while(!t.isInterrupted()){
+	            	LCD.clear();
+	            	int res;
+					File fichier=null;
+					if(Configurations.musique.getVal().equals(nameConfig)) {
+						fichier = new File(nameFile);
+						if (fichier==null) {
+							Sound.beep();
+							LCD.drawString("Fichier pas ouvert", 3, 2);
+							System.out.println("Fichier pas ouvert");
+							return; //on sort de la fonction si le fichier n'est pas trouve
+						}
+						res = Sound.playSample(fichier, Sound.VOL_MAX); //attention : instruction bloquante
+						LCD.clear();
+						if (res<0)
+							LCD.drawString("fichier.waw au mauvais format", 3, 4);
+					}
+					else {
+						System.out.println(nameConfig+" n'est pas la configuration choisie");
+					}
+					
+	            }
+			}
+		});
+		t.start();
 }
+	public static void stopMusic() {
+  	  t.interrupt(); //va tenter d'arreter le thread en cours d'execution
+    	}
+	}
+    
