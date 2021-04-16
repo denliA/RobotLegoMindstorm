42c73e286b673af89311d0168f300fd88756956c
ij07411 <ij07411@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-04-16 22:53:20 +0000

    Ajoutes des lancables pour les tests de la recette. Les test necessaires de la recettes ne sont plus adaptés car remplis par l'interface textuelle

--- trunk/src/tests/NFA1.java
+++ trunk/src/tests/NFA1.java
@@ -1,12 +1,12 @@
 package tests;
 
-public class P4 implements interfaceEmbarquee.Lancable{
+public class NFA1 implements interfaceEmbarquee.Lancable{
 	
 	public void lancer() {
 	}
 	
 	public String getTitre() {
-		return "P4";
+		return "NFA1";
 	}
 	
 }
--- trunk/src/tests/NFBM1.java
+++ trunk/src/tests/NFBM1.java
@@ -1,12 +1,12 @@
 package tests;
 
-public class P4 implements interfaceEmbarquee.Lancable{
+public class NFBM1 implements interfaceEmbarquee.Lancable{
 	
 	public void lancer() {
 	}
 	
 	public String getTitre() {
-		return "P4";
+		return "NFBM1";
 	}
 	
 }
