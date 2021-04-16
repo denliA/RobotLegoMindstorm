35bcf59ceec3f9b6bd2a76a76223ea7da9536c37
ij07411 <ij07411@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-04-16 23:37:10 +0000

    Quelques test faits

--- trunk/src/tests/NFBA1.java
+++ trunk/src/tests/NFBA1.java
@@ -1,8 +1,20 @@
 package tests;
 
+import capteurs.Capteur;
+import capteurs.Couleur;
+import moteurs.MouvementsBasiques;
+import moteurs.Pilote;
+
 public class NFBA1 implements interfaceEmbarquee.Lancable{
 	
 	public void lancer() {
+		new Capteur();
+		Couleur.startScanAtRate(0);
+		moteurs.MouvementsBasiques.chassis.travel(Double.NEGATIVE_INFINITY);
+		Couleur.blacheTouchee();
+		while(!Couleur.blacheTouchee());
+		MouvementsBasiques.chassis.stop();
+		MouvementsBasiques.chassis.waitComplete();
 	}
 	
 	public String getTitre() {
--- trunk/src/tests/NFBM4.java
+++ trunk/src/tests/NFBM4.java
@@ -1,9 +1,35 @@
 package tests;
 
+import capteurs.Couleur;
+import capteurs.CouleurLigne;
+import lejos.hardware.Button;
+import moteurs.MouvementsBasiques;
+import moteurs.Pilote;
+
 public class NFBM4 implements interfaceEmbarquee.Lancable{
 	
 	public void lancer() {
+		int button = -1;
+		Pilote.startVideAtRate(0);
+		Couleur.startScanAtRate(10);
+		while (button != Button.ID_ESCAPE) {
+			button = Button.waitForAnyPress();
+			if (button == Button.ID_ENTER) {
+				Couleur.videTouche();
+				MouvementsBasiques.chassis.travel(Double.POSITIVE_INFINITY);
+				while(!Couleur.videTouche()) {
+					//on continue d'avancer tout droit
+				}
+				MouvementsBasiques.chassis.stop(); MouvementsBasiques.chassis.waitComplete();
+				MouvementsBasiques.chassis.travel(-10); MouvementsBasiques.chassis.waitComplete();
+				MouvementsBasiques.chassis.rotate(180); MouvementsBasiques.chassis.waitComplete();
+			}
+			Button.waitForAnyEvent();
+		}
+		Pilote.stopVide();
+		Couleur.stopScan();
 	}
+	
 	
 	public String getTitre() {
 		return "NFBM4 - Detecter vide";
