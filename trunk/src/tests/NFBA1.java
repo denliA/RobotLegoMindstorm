200157020d98c98acf6c95d071f927a085ffae30
ij07411 <ij07411@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-04-17 02:21:06 +0000

    test NFA1

--- trunk/src/tests/NFBA1.java
+++ trunk/src/tests/NFBA1.java
@@ -3,14 +3,13 @@ package tests;
 import capteurs.Capteur;
 import capteurs.Couleur;
 import moteurs.MouvementsBasiques;
-import moteurs.Pilote;
 
 public class NFBA1 implements interfaceEmbarquee.Lancable{
 	
 	public void lancer() {
 		new Capteur();
 		Couleur.startScanAtRate(0);
-		moteurs.MouvementsBasiques.chassis.travel(Double.NEGATIVE_INFINITY);
+		moteurs.MouvementsBasiques.chassis.travel(Double.POSITIVE_INFINITY);
 		Couleur.blacheTouchee();
 		while(!Couleur.blacheTouchee());
 		MouvementsBasiques.chassis.stop();
