23b1e40504d48006c7d2352c273345a40a73230a
il09104 <il09104@7fd38ddb-c8a7-4d86-af29-336008b8f976>
2021-04-27 02:44:16 +0000

    rajout du dossier d'installation

---
+++ trunk/installation/.lejosimage/lejos/bin/install.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# install lejos on the new file system
+. ./funcs.sh
+rootfs=/media/rootfs
+bootfs=/media/bootfs
+mkdir $bootfs 2> /dev/null
+mkdir $rootfs 2> /dev/null
+mount $1 $bootfs
+mount $2 $rootfs
+LEJOS_HOME=$rootfs/home/root/lejos
+log "Prepare install"
+if [ ! -e $bootfs/lejosimage.bz2 ]
+then
+  error "Missing lejos install files"
+fi
+if [ ! -e $bootfs/ejre* ]
+then
+  error "Missing jre install files"
+fi
+log "Deleting old files"
+rm -rf $rootfs/*
+log "Expand image"
+tar -C $rootfs -jxf $bootfs/lejosimage.bz2
+if [ ! -e $rootfs/lejosimage ]
+then
+  error "Missing lejos image"
+fi
+current=${PWD}
+cd $rootfs/lejosimage
+log "Start install"
+./update_sdcard.sh $bootfs $rootfs $current
+# début installation programme L2A2
+cp $bootfs/InterfaceTextuelle.jar $rootfs /home/lejos/programs/
+echo "lejos.default_program = /home/lejos/programs/InterfaceTextuelle.jar" >> /home/root/lejos/settings.properties
+echo "lejos.default_autoRun = ON" >> /home/root/lejos/settings.properties
+# fin installation programme L2A2
+log "Remove temp files"
+cd $rootfs
+rm -rf lejosimage
+cd /
+log "Configure boot"
+mv $bootfs/boot.scr $bootfs/setup.scr
+log "Sync disks"
+sync
+sync
+log "Unmount disks"
+umount $rootfs
+umount $bootfs
