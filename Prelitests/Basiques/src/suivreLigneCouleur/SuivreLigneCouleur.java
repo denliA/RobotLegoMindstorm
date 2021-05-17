37202097c6fd86bd0d0c24c756348cad5f014958
il09104 <30708312+DaBlumer@users.noreply.github.com>
2021-05-17 22:08:32 +0000

    Création d'un tag pour les Prelitests effectués en début de semestre, et suppression du dossier Prelitests du trunk pour ne pas le confondre avec le vrai code en src pendant l'évaluation

---
+++ Prelitests/Basiques/src/suivreLigneCouleur/SuivreLigneCouleur.java
@@ -0,0 +1,195 @@
+package suivreLigneCouleur;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.freedesktop.dbus.bin.IdentifierMangler;
+
+import deplacer.Droit;
+import deplacer.Tourner;
+import palet.*;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.Sound;
+import lejos.utility.Delay;
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
+
+public class SuivreLigneCouleur {
+	final static Port COLOR_SENSOR_PORT = LocalEV3.get().getPort("S3"); // Port de branchement du capteur de couleurs
+	final static float INCREMENT = 1.0f; // pas entre chaque mesure
+    final static float LONG_LIGNE = 174.0f; // longueur de la partie mesurée
+    static EV3ColorSensor color_sensor = new EV3ColorSensor(COLOR_SENSOR_PORT); 
+    public static SampleProvider RGB = color_sensor.getRGBMode(); 
+    public static SampleProvider AmbientLightMode = color_sensor.getAmbientMode();
+    public static SampleProvider RedMode = color_sensor.getRedMode();
+    public static SampleProvider ID = color_sensor.getColorIDMode();
+    static float value[] = new float[RGB.sampleSize()+3];
+    static FileWriter outputer = null;
+    
+    final static float targetLigneRougeR = 1.45f;
+    //26f pour entre deux rouge inteval// a mesurer le rgb sur bord ligne
+	final static float KP = 1f; //if small, robot smooth turns
+	final static float KI = 0.5f;
+	final static float KD = 1f;
+	static float DEFAULT_SPEED = Droit.D.getSpeed();
+	static float MAX_SPEED = Droit.D.getMaxSpeed();
+	static float ERROR_MARGIN = 0.1f;
+    
+	
+	public static void mesurerCouleurAff(){
+		/* Prise de la mesure */
+        RGB.fetchSample(value, 0); 
+        AmbientLightMode.fetchSample(value, 3);
+        RedMode.fetchSample(value, 4);
+        ID.fetchSample(value, 5);
+        /* Affichage de la dernière mesure sur l'écran du robot  */
+        LCD.clear();
+		LCD.drawString("R : "+value[0]*255, 0, 4);
+		LCD.drawString("G : "+value[1]*255, 0, 5);
+		LCD.drawString("B : "+value[2]*255, 0, 6);
+		LCD.drawString("AMB : "+value[3], 0, 3);
+		LCD.drawString("RedIntensity : "+value[4], 0, 2);
+		LCD.drawString("Color : "+value[5], 0, 0);
+		//System.out.println("R: "+value[0]*255+"\tG :"+value[1]*255+"\tB : "+value[2]*255);
+	}
+	
+	public static void mesurerCouleurFich() {
+		mesurerCouleurAff();
+		/* Ecriture des mesures dans le fichier déjà ouvert */
+		try {
+			outputer.write(value[0]*255 + "\t"+value[1]*255+"\t"+value[2]*255+ 
+					"\t" + value[3] + "\t" + value[4] + "\t" + value[5] + "\n");
+		} catch (IOException e) {
+			System.err.println("Erreur lors de l'écriture de la mesure!");
+			e.printStackTrace();
+		}	
+	}
+	
+	public static void stopMesure() {
+		color_sensor.close();
+	}
+	
+	public static void creerFichier(String nomFichier) {
+		/* Création du fichier */
+        try {
+			outputer = new FileWriter(nomFichier);	
+		} catch (IOException e) {
+			System.err.println("Erreur à l'ouverture du fichier!");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void metadataFichier(int vitesse, int acceleration, int delai, int roue) {
+		try {
+			outputer.write(vitesse+"°/s" + "\t" + acceleration+"°/s²"+ "\t" + roue+"pi*mm/360°"+ "\t" + delai+"ms/snap" + "\n");
+			outputer.write("R\tG\tB\tAmbient\tRedMode\tID\n");
+		} catch (IOException e) {
+			System.err.println("Erreur lors de l'écriture des métadonnées!");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void fermerFichier() {
+		try {
+			outputer.close();
+		} catch (IOException e) {
+			System.err.println("Erreur à la fermeture du fichier");
+			e.printStackTrace();
+		}
+	}
+	
+	public static void Ligne(float[] bornes) {
+			
+		//float Lignejaune_BleuMin = 35; guillaume
+		//float Lignejaune_BleuMax = 43;
+		int DEFAULT_SPEED = Droit.G.getSpeed();
+		long debut;
+		//Prise de la mesure 
+        RGB.fetchSample(value, 0); 
+        //Verification que robot ne sort pas de sa ligne
+        if (value[2]*255<bornes[4] || value[2]*255>bornes[5] || value[1]*255<bornes[2] || value[1]*255>bornes[3] || value[0]*255<bornes[0] || value[0]*255>bornes[1]) {
+        	Sound.twoBeeps();
+        	debut = System.currentTimeMillis();
+        	Droit.G.setSpeed((float)(DEFAULT_SPEED*1.2));
+        	while ((value[2]*255<bornes[4] || value[2]*255>bornes[5] || value[1]*255<bornes[2] || value[1]*255>bornes[3] || value[0]*255<bornes[0] || value[0]*255>bornes[1]) && System.currentTimeMillis() - debut < 250) {
+        		RGB.fetchSample(value, 0);
+        	}
+        	Droit.G.setSpeed(DEFAULT_SPEED);
+        }
+        RGB.fetchSample(value, 0); 
+        if (value[2]*255<bornes[4] || value[2]*255>bornes[5] || value[1]*255<bornes[2] || value[1]*255>bornes[3] || value[0]*255<bornes[0] || value[0]*255>bornes[1]) {
+        	Sound.beep();
+        	debut = System.currentTimeMillis();
+        	Droit.D.setSpeed((float)(DEFAULT_SPEED*1.2));
+        	while ((value[2]*255<bornes[4] || value[2]*255>bornes[5] || value[1]*255<bornes[2] || value[1]*255>bornes[3] || value[0]*255<bornes[0] || value[0]*255>bornes[1]) && System.currentTimeMillis() - debut < 500) {
+        		RGB.fetchSample(value, 0);
+        	}
+        	Droit.D.setSpeed(DEFAULT_SPEED);
+        }
+	}
+	
+	public static void Ligne_PID(float target, float DEFAULT_SPEED) {
+		float error = 0f;
+		float previousError = 0f;
+		float integral = 0f;
+		float derivative = 0f;
+		float correction = 0f;
+		do {
+			RGB.fetchSample(value, 0); //input de la couleur mesurée
+			error = (target - (value[0]/value[1]));
+			if (Math.abs(error)>ERROR_MARGIN) { //on ne fait rien si l'erreur est negligeable
+				integral += error;
+				derivative = error - previousError;
+				correction = (error * KP)+(integral * KI)+(derivative * KD); //PID control
+				previousError = error;
+				// limiter le output 
+				if (correction>MAX_SPEED)
+					correction = MAX_SPEED;
+				else if (correction<-MAX_SPEED)
+					correction = -MAX_SPEED;
+				// tourner robot
+				Droit.G.setSpeed(DEFAULT_SPEED+correction);
+				Droit.D.setSpeed(DEFAULT_SPEED-correction);
+			}
+		}while(Math.abs(error)>ERROR_MARGIN); // sortie de boucle quand robot s'est redressé sur l'entre ligne rouge-grise
+		//robot avance tout droit 
+		Droit.G.setSpeed(DEFAULT_SPEED);
+		Droit.D.setSpeed(DEFAULT_SPEED);
+	}
+			/*
+			if (error<0) { // robot sur ligne rouge
+				Droit.G.setSpeed(DEFAULT_SPEED+correction);
+				Droit.D.setSpeed(DEFAULT_SPEED-correction);
+			}
+			else if (error>0) { // robot sur zone grise
+				Droit.D.setSpeed(DEFAULT_SPEED+correction);
+				Droit.G.setSpeed(DEFAULT_SPEED-correction);
+			}*/
+			//Delay.msDelay();
+	
+		/*
+		}while(Math.abs(error)>ERROR_MARGIN); // sortie de boucle quand robot s'est redressé sur l'entre ligne rouge-grise
+		//robot avance tout droit 
+		Droit.G.setSpeed(DEFAULT_SPEED);
+		Droit.D.setSpeed(DEFAULT_SPEED);	
+		*/
+	
+	public static void ramenerPaletSolo() throws Exception {
+		boolean res = DetecterPalet.detecterPalet();
+		if (res) {
+			MainSuivreLigne.mPalet = System.currentTimeMillis();
+		}
+	}
+	
+	public static void ramenerPaletSoloPID() throws Exception {
+		boolean res = DetecterPalet.detecterPalet();
+		if (res) {
+			//Main_PID_LineFollower.foundPalet = true;
+		}
+	}
+}
